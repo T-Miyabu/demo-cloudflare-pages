235c64dea86deded8ced33dd7b3e00e8a79230cb
“T-Miyabu” <takumi.miyabu@mediagene.co.jp>
2023-01-30 17:57:02 +0900

    middleware修正

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
+import type { NextRequest, NextFetchEvent } from 'next/server';
+
 import { NextResponse } from 'next/server';
 
-export function middleware() {
+export function middleware(req: NextRequest, event: NextFetchEvent) {
   const response = NextResponse.next();
   response.headers.set('Cache-Control', 'max-age=0, s-maxage=60, stale-if-error=60');
   return response;
